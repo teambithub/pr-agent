3b8894bc38a224e56a6c57c238bae1ea40934a85
Pablo Viojo <pviojo@gmail.com>
2024-09-23 18:17:22 -0300

    Add pr_url arg and README

--- pr_agent/exec.py
+++ pr_agent/exec.py
@@ -18,15 +18,30 @@ log_level = os.environ.get("LOG_LEVEL", "DEBUG")
 setup_logger(level=log_level, fmt=LoggingFormat.CONSOLE)
 
 
-def run():
+def set_parser():
+    parser = argparse.ArgumentParser(
+        description='AI based pull request analyzer')
+    parser.add_argument('--pr_url', type=str,
+                        help='The URL of the PR to review', default=None)
+    return parser
+
+
+def run(inargs=None, args=None):
+    parser = set_parser()
+    if not args:
+        args = parser.parse_args(inargs)
+    if not args.pr_url and not args.issue_url:
+        print('No --pr_url argument passed')
+        return
+
     get_settings().set("CONFIG.CLI_MODE", False)
     get_settings().set("CONFIG.VERBOSITY_LEVEL", 2)
+    print('>>>> args.pr_url', args.pr_url)
 
     async def inner():
 
-        pr_url = "https://github.com/teambithub/git-service/pull/33"
         ai_handler = LiteLLMAIHandler  # will be initialized in run_action
-        obj = PRReviewer(pr_url=pr_url, ai_handler=ai_handler)
+        obj = PRReviewer(pr_url=args.pr_url, ai_handler=ai_handler)
         await asyncio.create_task(obj.run())
         return obj.data if hasattr(obj, "data") else None
 
